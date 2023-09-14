a9316b059b2cf94781f40b0bda904de0544a1071
KevynG98 <kgprogrammer98@gmail.com>
2023-09-14 12:22:58 -0600

    Primer commit

---
+++ src/compiladores/Compiladores.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package compiladores;
+
+/**
+ *
+ * @author kevyn
+ */
+public class Compiladores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana miVentana = new Ventana();
+        miVentana.setVisible(true);
+        miVentana.setResizable(false);
+        miVentana.setLocationRelativeTo(null);
+        // TODO code application logic here
+    }
+    
+}
